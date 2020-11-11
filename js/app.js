9edac2e7c3963994f678dc1b3eb8f9e3bbe0d32d
tediko <tediko123@wp.pl>
2020-11-11 11:54:47 +0100

    Added addAnimation and removeAnimation functions

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ let references = [
         image: 'assets/img/image-john.jpg'
     }
 ]
-
+const sliderWrapper = document.querySelector('.slider__wrapper');
 const buttons = document.querySelectorAll('[data-btn]');
 const reference = document.querySelector('[data-ref]');
 const name = document.querySelector('[data-name]');
@@ -22,14 +22,25 @@ const title = document.querySelector('[data-title]');
 const image = document.querySelector('[data-image]');
 let count = 0;
 
+const addAnimation = () => {
+    name.style.animation = `authorSlide 1000ms`;
+    title.style.animation = `authorSlide 1000ms`;
+    reference.style.animation = `refSlide 1000ms`;
+    image.style.animation = `imgSlide 1000ms`;
+}
+
 const displaySlide = (idx) => {
     name.textContent = references[idx].name;
     title.textContent = references[idx].title;
     reference.textContent = references[idx].reference;
     image.src = references[idx].image;
+    addAnimation();
 }
 
 const slider = (element) => {
+    buttons.forEach(button => {
+        button.style.pointerEvents = 'none';
+    })
     const target = element.target.dataset.btn;
     const sliderLength = references.length - 1;
     target == 'next' ? count++ : count--;
@@ -38,6 +49,20 @@ const slider = (element) => {
     displaySlide(count);
 }
 
-buttons.forEach(element => {
-    element.addEventListener('click', slider);
+const removeAnimation = () => {
+    reference.style.animation = ``;
+    image.style.animation = ``;
+    name.style.animation = ``;
+    title.style.animation = ``;
+}
+
+buttons.forEach(button => {
+    button.addEventListener('click', slider);
+})
+
+sliderWrapper.addEventListener('animationend', () => {
+    buttons.forEach(button => {
+        button.style.pointerEvents = 'auto';
+    })
+    removeAnimation();
 })
